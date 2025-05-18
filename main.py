90bc0e0781d3d8f73e6678413d6dd95e7cc01383
Georgy Sorokin <georgywebdev@gmail.com>
2025-05-18 15:23:19 +0300

    .env and local db connection

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
+load_dotenv()
+
 api = FastAPI()
+
+url = os.getenv('DATABASE_URL')
+connection = psycopg2.connect(url)
 
 
 @api.get("/")
